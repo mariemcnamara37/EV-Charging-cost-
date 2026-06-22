e71de8d1562ef1125bb899edd67e87c52a1bb169
mariemcnamara37 <mmcnamara@rmi.org>
2026-06-22 13:23:54 -0600

    removed line km as toggle
    set to default 1km

--- ev_charger_model_app.py
+++ ev_charger_model_app.py
@@ -216,9 +216,6 @@ with st.sidebar:
 
     charger_quantity = st.slider("Number of chargers", min_value=1, max_value=50, value=10, step=1)
 
-    line_km = st.slider("Grid line extension (km)", min_value=0.0, max_value=20.0, value=1.0, step=0.5)
-    st.markdown('<div class="explainer">Distance to nearest transformer or substation. Longer runs increase civil & cable costs.</div>', unsafe_allow_html=True)
-
     daily_util_hrs = st.slider("Daily utilization (hrs/day)", min_value=1.0, max_value=20.0, value=5.0, step=0.5)
     st.markdown('<div class="explainer">Average hours each charger actively dispenses power per day.</div>', unsafe_allow_html=True)
 
@@ -236,7 +233,7 @@ with st.sidebar:
 m = run_model(
     charger_size=charger_size,
     charger_quantity=charger_quantity,
-    line_km=line_km,
+    line_km=1.0,
     daily_util_hrs=daily_util_hrs,
     electricity_sale_price_usd=electricity_sale_price_usd,
     selected_state=selected_state,
